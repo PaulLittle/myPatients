e34a0833aecc8855db87f39983238aa65b973c6f
PaulLittle <S00156762@mail.itsligo.ie>
2015-11-05 22:20:51 +0000

    1st attempt

--- myPatients/MainWindow.xaml.cs
+++ myPatients/MainWindow.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿/*
+    Author:         Paul Little
+    Student No:     S00156762
+    Description:    FA2 Patient Blood Types
+    Date:           05-11-2015
+*/
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +27,89 @@ namespace myPatients
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random(System.Environment.TickCount);
+
+        ObservableCollection<Patient> group = new ObservableCollection<Patient>();
+        ObservableCollection<Patient> filteredGroup = new ObservableCollection<Patient>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // make a group of patients
+            PopulateGroup();
+            PopulateListbox();
+        }
+        
+        private void PopulateGroup()
+        {
+            group = new ObservableCollection<Patient>()
+            {
+                // Can this be done in a loop or generated randomly?
+                new Patient {Name = "John Smith", Blood = BloodGroup.A },
+                new Patient {Name = "Vivion Kinsella", Blood = BloodGroup.B },
+                new Patient {Name = "Una L'Estrange", Blood = BloodGroup.AB },
+                new Patient {Name = "Colm Davey", Blood = BloodGroup.AB },
+                new Patient {Name = "Paul Powell", Blood = BloodGroup.A },
+                new Patient {Name = "Aine Mitchell", Blood = BloodGroup.AB },
+                new Patient {Name = "Fergal Keane", Blood = BloodGroup.B },
+                new Patient {Name = "Fran Regan", Blood = BloodGroup.AB },
+                new Patient {Name = "Padraig Harte", Blood = BloodGroup.AB },
+                new Patient {Name = "Neil Gannon", Blood = BloodGroup.A },
+                new Patient {Name = "Drew Lang" },
+                new Patient()
+            };
+        }
+
+        private void PopulateListbox()
+        {
+            FilterByBloodGroup('A');
+            lbxPatients.ItemsSource = filteredGroup;
+        }
+
+        private void lbxPatients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Notice2.Text = (lbxPatients.SelectedIndex >= 0) ? (lbxPatients.SelectedItem as Patient).Name : "Nothing Selected";
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var button = sender as RadioButton;
+            if (button.Content == null) return;
+            char filterBlood = Char.Parse(button.Content.ToString());
+            FilterByBloodGroup(filterBlood);
+        }
+
+        private void FilterByBloodGroup(char blood)
+        {
+            filteredGroup.Clear();            
+
+            foreach (var patient in group)
+            {
+                if (patient.Blood == BloodGroup.A && blood == 'A')
+                {
+                    filteredGroup.Add(patient);
+                    Notice1.Text = "BLOOD TYPE A";
+                }
+                else if (patient.Blood == BloodGroup.AB && blood == 'Y')
+                {
+                    filteredGroup.Add(patient);
+                    Notice1.Text = "BLOOD TYPE AB";
+                }
+                else if (patient.Blood == BloodGroup.B && blood == 'B')
+                {
+                    filteredGroup.Add(patient);
+                    Notice1.Text = "BLOOD TYPE B";
+                }
+                else if (blood == 'Z')
+                {
+                    filteredGroup.Add(patient);
+                    Notice1.Text = "ALL BLOOD TYPES";
+                }
+            }
+        }
+    }  // end class
+}  // end ns
---
+++ myPatients/Patient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myPatients
+{
+    public enum BloodGroup { A, AB, B, Z}
+
+    class Patient
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        private BloodGroup _blood;
+        public BloodGroup Blood { get { return _blood; } set { _blood = value; } }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0} <==> Blood Type: {1}", Name, Blood);
+        }
+
+        public Patient():this("John Kelleher", BloodGroup.A)
+        {}
+
+        public Patient(string nme)
+        {
+            Name = nme;
+        }
+
+        public Patient(string nme, BloodGroup bg)
+        {
+            Name = nme;
+            Blood = bg;
+        }
+    }
+}
